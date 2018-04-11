a0fea63636a5c682acd417d4a7d08a12e3deb919
neuRowsATL <bchung808@gmail.com>
2018-04-11 13:01:00 -0400

    Building SAUCY object for single file analysis

--- matSAUCY/SAUCY.m
+++ matSAUCY/SAUCY.m
@@ -1,11 +1,32 @@
-classdef SAUCY
-    %UNTITLED11 Summary of this class goes here
-    %   Detailed explanation goes here
+classdef SAUCY < handle
+    % SAUCY Object class to manage analysis of single file
+    %   SAUCY allows analysis for a single file and can be used for
+    %   threshold detection, PCA classification, and optimization of false
+    %   positives and false negatives.
     
     properties
+        experiment_name = '' % base name of experiment
+        
+        fname_intan = '' % full file name of intan file
+        fname_cbin = '' % full file name of cbin file
+        fname_neuralnot = '' % full file name of neural_not file
+        
+        % chan          Channel number(s) for analysis
+        %   as INT      Single channel will be used
+        %   as TUPLE    Channel subtraction will be used (a b) = a - b
+        chan = []
+        
+        n_clusters = 2 % number of clusters to detect
+        
+        Fs = 30000 % sampling rate of recording
+        
+        data = {} % Struct to store data
     end
     
     methods
+        function load_data()
+           
+        end
     end
     
 end
