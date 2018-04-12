9946f5d7c66ac3caede31c1dcfc695a1aba2f813
neuRowsATL <bchung4@student.gsu.edu>
2018-04-12 16:49:09 -0400

    simplified commands

--- matSAUCY/test_SAUCY.m
+++ matSAUCY/test_SAUCY.m
@@ -2,8 +2,10 @@
 mySaucy = SAUCY('bl21lb21_171218', 8);
 
 load_data(mySaucy, 'bl21lb21_171218_134533.rhd');
-filter_data(mySaucy);
-set_threshold(mySaucy, 'filt');
+do_saucy(mySaucy);
 
-set_spike_mat(mySaucy, 'threshold');
-optimize_clusters(mySaucy);
+% filter_data(mySaucy);
+% set_threshold(mySaucy, 'filt');
+% 
+% set_spike_mat(mySaucy, 'threshold');
+% optimize_clusters(mySaucy);
