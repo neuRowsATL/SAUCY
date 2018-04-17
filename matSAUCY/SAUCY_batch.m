e7e0e9fccfa05d3819266c0cd21e08c8980d5302
neuRowsATL <bchung808@gmail.com>
2018-04-17 13:02:22 -0400

    Renamed meta_SAUCY to SAUCY_batch

---
+++ matSAUCY/SAUCY_batch.m
@@ -0,0 +1,19 @@
+classdef meta_SAUCY < handle
+    %meta_SAUCY Manages multiple SAUCY objects for analysis
+    %   Large datasets can be managed across multiple data files using
+    %   meta_SAUCY
+    
+    properties
+        % mode      What mode to run analysis?
+        %   batch       Run across multiple files simultaneously
+        %   track       Track analysis results across multiple files
+        %   check       Check analysis results
+        %   add_latent  Add latent values to analysis results?
+        mode = 'batch'
+    end
+    
+    methods
+    end
+    
+end
+
