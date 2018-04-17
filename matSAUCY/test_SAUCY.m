ca0a40bfef0f5049b72114c4ff7ca6e1c8c34151
neuRowsATL <bchung808@gmail.com>
2018-04-17 12:49:52 -0400

    Added information about how to use the SAUCY class.

--- matSAUCY/test_SAUCY.m
+++ matSAUCY/test_SAUCY.m
@@ -1,11 +1,31 @@
-% Initiate SAUCY object for experiment: bl21lb21_171218
+%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%% BC - April 17, 2018
+%%%%% This is a test script that uses data recorded from an experiment that
+%%%%% was named "bl21lb21_171218" with the data channel of interest #8. The
+%%%%% raw data file recorded using Intan was saved to
+%%%%% "bl21lb21_171218_134533.rhd" and is available in the working path of
+%%%%% MATLAB.
+%%%%%
+%%%%% Because the data file is larger than github's allowable filesize,
+%%%%% please contact the author of this script at bpchung[at]emory.edu.
+%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%% Initiate SAUCY object for experiment: bl21lb21_171218
 mySaucy = SAUCY('bl21lb21_171218', 8);
-
 load_data(mySaucy, 'bl21lb21_171218_134533.rhd');
-do_saucy(mySaucy);
+filter_data(mySaucy);
 
-% filter_data(mySaucy);
-% set_threshold(mySaucy, 'filt');
-% 
-% set_spike_mat(mySaucy, 'threshold');
-% optimize_clusters(mySaucy);
+%%%%% Run full SAUCY default analysis
+%%%%% set_threshold(S);
+%%%%% do_clustering(S);
+%%%%% optimize_clusters(S);
+% do_saucy(mySaucy);
+
+%%%%% Load spike times and then run SAUCY clustering
+load('bl21lb21_171218_CH8_spikes-th.mat');
+mySaucy.threshold = 80;
+do_clustering(mySaucy, 2, round(ts*30000));
+optimize_clusters(mySaucy);
